3c234fb578c98f011c5bffb761ce496db0067f30
xiao.geng <xiao.geng@relxtech.com>
2024-03-04 09:21:27 +0800

    feat: update

--- exam/2023-12/GESP 五级/编程题/02.烹饪问题/case/code.cpp
+++ exam/2023-12/GESP 五级/编程题/02.烹饪问题/case/code.cpp
@@ -13,17 +13,27 @@ const int MAX_N = int(1e6) + 100;
 int a[MAX_N];	
 
 int sort(int right, int bit) {
+    cout << "right  bit: " << right << "-->" << bit << endl;	
     int left = 1;
     while(left <= right) {	
-        //  如果 left 左移 bit 位后，& 1 不为 0，left++
+        // cout << "right  bit  a[left]  a[right]: " << right << "-->" << bit  << "-->" << a[left] << "-->" << a[right] << endl;
+        // cout << "b1: " << (a[left] >> bit) << "--" << ((a[left] >> bit) & 1) << endl;
+        // cout << "b2: " << (a[right] >> bit) << "--" << ((a[right] >> bit) & 1) << endl;
+       
         while ((left <= right) && ((a[left] >> bit) & 1)) left++;
-         //  如果 right 左移  bit 位后，& 1 不为 0，right--
         while ((left <= right) && (!((a[right] >> bit) & 1))) right--;
-        // 交互 left  和 right
+
+        // cout << "left right: " << left << "--" << right << endl;
         if (left <= right) {
             swap(a[left++], a[right--]);
         };
+
+        // for (int i = 1; i <= 5; i++) {
+        //     printf("%d", a[i]);
+        // }
+        // cout <<  endl;
     }
+    // cout << "right:" << right <<  endl;
     return right;	
 }
 
@@ -37,12 +47,17 @@ int main() {
     }
 
     for (int i = 31; i >= 0; i--)	{
-        // sort(n,i) 返回的 right 大于2，说明前2位的 & 不为 0，记录下来存放到 ans 中
+        cout << "n  i: " << n << "-->" << i << endl;
         if (((j = sort(n,i))) >= 2) {	
+            // cout << "j:" << j <<  endl;
+            // cout << "1 << i :" << 1 << i <<  endl;
+            // cout << "ans1 :" << ans <<  endl;
             ans = ans | (1 << i);	
-            // 下次处理时候，忽略掉已处理的数据
+            // cout << "ans2 :" << ans <<  endl;
   	        n = j;	
         }
+        // cout << "n :" << n <<  endl;
+        // cout << "#########" << endl;
     }
 
     printf("%d\n", ans);	
